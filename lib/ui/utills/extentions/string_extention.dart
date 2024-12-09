7c8363033355b283b43b2fc684ec8b5a53d05994
muhammed-ajmal1217 <stranger99980@gmail.com>
2024-12-09 16:56:49 +0530

    some changes

--- lib/ui/utills/extentions/string_extention.dart
+++ lib/ui/utills/extentions/string_extention.dart
@@ -1,6 +1,11 @@
 extension StringExtension on String {
   String get nkStringCapitalizeFirstCaracter {
-    return "${this[0].toUpperCase()}${substring(1)}";
+    if (this.isEmpty) {
+    return this;
+  }
+  return this.length > 0
+      ? "${this[0].toUpperCase()}${substring(1)}" 
+      : this.toUpperCase();
   }
 
   String get nkValueWithCurrencySymbol {
@@ -12,9 +17,7 @@ extension StringExtension on String {
 }
 
 String formatAmount(dynamic value) {
-  // Convert the dynamic value to double
   double amount;
-
   if (value is String) {
     amount = double.tryParse(value) ?? 0.0;
   } else if (value is int) {
@@ -24,11 +27,7 @@ String formatAmount(dynamic value) {
   } else {
     throw ArgumentError('Unsupported value type');
   }
-
-  // Format the amount to two decimal places
   String formattedAmount = amount.toStringAsFixed(2);
-
-  // Return with the $ symbol
   return '\$ ' + formattedAmount;
 }
 

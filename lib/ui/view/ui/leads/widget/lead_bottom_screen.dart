38c0792fc8000d9ac39f3b3555adcd8afad2a30c
muhammed-ajmal1217 <stranger99980@gmail.com>
2024-12-31 14:00:46 +0530

    some changes

--- lib/ui/view/ui/leads/widget/lead_bottom_screen.dart
+++ lib/ui/view/ui/leads/widget/lead_bottom_screen.dart
@@ -18,45 +18,191 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class LeadBottomScreen extends StatelessWidget {
+class LeadBottomScreen extends StatefulWidget {
   final LeadsController leadsController;
   const LeadBottomScreen({super.key, required this.leadsController});
+
+  @override
+  State<LeadBottomScreen> createState() => _LeadBottomScreenState();
+}
+
+class _LeadBottomScreenState extends State<LeadBottomScreen> {
+  final ScrollController vertical = ScrollController();
+  final ScrollController vertical1 = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    vertical.addListener(() {
+      if (vertical1.hasClients &&
+          vertical.position.pixels != vertical1.position.pixels) {
+        vertical1.jumpTo(vertical.position.pixels);
+      }
+    });
+
+    vertical1.addListener(() {
+      if (vertical.hasClients &&
+          vertical1.position.pixels != vertical.position.pixels) {
+        vertical.jumpTo(vertical1.position.pixels);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    bool isLandscape =
+        MediaQuery.of(context).orientation == Orientation.landscape;
+    double fixedRowHeight = isLandscape
+        ? MediaQuery.of(context).size.height / 9.05
+        : MediaQuery.of(context).size.height / 9 -
+            MediaQuery.of(context).size.height * 0.032;
+
     return MyCommnonContainer(
       padding: EdgeInsets.zero,
       child: Obx(() {
-        return _buildTableLayout(context);
+        return _buildTableLayout(context, fixedRowHeight);
       }),
     );
   }
 
-  Widget _buildTableLayout(BuildContext context) {
-    return Column(
-      children: [
-        _buildTableHeader(),
-        leadsController.leadsCustomerDataList.isEmpty
-            ? SizedBox(height: MediaQuery.of(context).size.height * 0.4)
-            : Container(),
-        leadsController.leadsCustomerDataList.isEmpty
-            ? Center(
-                child: NodataWidget(),
-              )
-            : Expanded(
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: leadsController.leadsCustomerDataList
-                        .asMap()
-                        .entries
-                        .map((entry) {
-                      int index = entry.key;
-                      LeadCustomerData leadCustomerData = entry.value;
-                      return _buildTableRow(leadCustomerData, context, index);
-                    }).toList(),
+  Widget _buildTableLayout(BuildContext context, double fixedRowHeight) {
+    double totalTableWidth = 120 + 350 + 140 + 140 + 140 + 140 + 160 + 100;
+    return Container(
+      child: Row(
+        children: [
+          SizedBox(
+            width: 270,
+            child: Column(
+              children: [
+                _buildTableHeader1(
+                  Center(
+                    child: CustomText(
+                      content: "Leads",
+                      textAlign: TextAlign.center,
+                      fontSize: 12,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  270,
+                ),
+                Expanded(
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.vertical,
+                    controller: vertical,
+                    physics: const ClampingScrollPhysics(),
+                    child: Column(
+                      children: widget.leadsController.leadsCustomerDataList
+                          .asMap()
+                          .entries
+                          .map((entry) {
+                        int index = entry.key;
+                        LeadCustomerData leadCustomerData = entry.value;
+
+                        return Container(
+                          height: fixedRowHeight,
+                          color: index.isEven ? Colors.grey[50] : Colors.white,
+                          
+                          child: Row(
+                            children: [
+                              Expanded(
+                                flex: 2,
+                                child: Row(
+                                  children: [
+                                    ClipOval(
+                                      child: Container(
+                                        height: 40,
+                                        width: 40,
+                                        color: Colors.grey[200],
+                                        child: Image.network(
+                                          'http://16.50.232.153:3000/uploads/${leadCustomerData.imageUrl ?? ''}',
+                                          fit: BoxFit.cover,
+                                          width: 25,
+                                          height: 25,
+                                          errorBuilder: (context, error, stackTrace) {
+                                            return Container(
+                                              color: Colors.grey[200],
+                                              child: const Icon(
+                                                Icons.person,
+                                                color: Colors.blue,
+                                                size: 34,
+                                              ),
+                                            );
+                                          },
+                                        ),
+                                      ),
+                                    ),
+                                    const SizedBox(width: 10),
+                                    CustomText(
+                                      content:
+                                          leadCustomerData.businessName ?? '',
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.bold,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      }).toList(),
+                    ),
                   ),
                 ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: SizedBox(
+                width: totalTableWidth,
+                child: Column(
+                  children: [
+                    SizedBox(child: _buildTableHeader()),
+                    widget.leadsController.leadsCustomerDataList.isEmpty
+                        ? SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.4)
+                        : Container(),
+                    widget.leadsController.leadsCustomerDataList.isEmpty
+                        ? Center(child: NodataWidget())
+                        : SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.8,
+                            child: SingleChildScrollView(
+                              controller: vertical1,
+                              scrollDirection: Axis.vertical,
+                              child: Column(
+                                children: widget
+                                    .leadsController.leadsCustomerDataList
+                                    .asMap()
+                                    .entries
+                                    .map((entry) {
+                                  int index = entry.key;
+                                  LeadCustomerData leadCustomerData = entry.value;
+                                  return _buildTableRow(
+                                      leadCustomerData, context, index,fixedRowHeight);
+                                }).toList(),
+                              ),
+                            ),
+                          ),
+                  ],
+                ),
               ),
-      ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTableHeader1(Widget child, double width) {
+    return Container(
+      width: width,
+      alignment: Alignment.center,
+      color: primaryColor,
+      padding: const EdgeInsets.symmetric(vertical: 11),
+      child: child,
     );
   }
 
@@ -67,13 +213,11 @@ class LeadBottomScreen extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          SizedBox(width: 10),
-          Expanded(flex: 2, child: _buildHeaderText('Leads', 12)),
-          Expanded(flex: 2,child: _buildHeaderText('Address', 12)),
+          Expanded(flex: 2, child: _buildHeaderText('Address', 12)),
           Expanded(child: _buildHeaderText('Mobile', 12)),
           Expanded(child: _buildHeaderText('Email', 12)),
-          Expanded(child: _buildHeaderText('C. Person', 12)),
-          Expanded(child: _buildHeaderText('C. Number', 12)),
+          Expanded(child: _buildHeaderText('Contact Person', 12)),
+          Expanded(child: _buildHeaderText('Contact Number', 12)),
           Expanded(child: _buildHeaderText('Status', 12)),
           const Expanded(child: Text('')),
         ],
@@ -94,134 +238,75 @@ class LeadBottomScreen extends StatelessWidget {
   }
 
   Widget _buildTableRow(
-      LeadCustomerData leadCustomerData, BuildContext context, int index) {
+      LeadCustomerData leadCustomerData, BuildContext context, int index,double fixedRowHeight) {
     return Container(
       color: index.isEven ? Colors.grey[50] : Colors.white,
-      padding: const EdgeInsets.all(10.0),
+      height: fixedRowHeight,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Expanded(
             flex: 2,
-            child: Row(
-              children: [
-                ClipOval(
-                  child: Container(
-                    height: 40,
-                    width: 40,
-                    color: Colors.grey[200],
-                    child: Image.network(
-                      'http://16.50.232.153:3000/uploads/${leadCustomerData.imageUrl ?? ''}',
-                      fit: BoxFit.cover,
-                      width: 25,
-                      height: 25,
-                      errorBuilder: (context, error, stackTrace) {
-                        return Container(
-                          color: Colors.grey[200],
-                          child: const Icon(Icons.person,
-                              color: Colors.blue, size: 34),
-                        );
-                      },
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 10),
-                Expanded(
+            child: CustomText(
+                content: '${leadCustomerData.address ?? ''} , ${leadCustomerData.town ?? ''} , ${leadCustomerData.state ?? ''} , ${leadCustomerData.zipcode?.toString() ?? ''}',
+                fontSize: 12,
+                maxLine: 2,
+                textAlign: TextAlign.center,
+                overflow: TextOverflow.ellipsis),
+          ),
+          Expanded(
+              child: Center(
                   child: CustomText(
-                    content: leadCustomerData.businessName ?? '',
-                    fontSize: 12,
-                    fontWeight: FontWeight.bold,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ),
-              ],
-            ),
-          ),
+                      content: leadCustomerData.businessNo ?? '',
+                      fontSize: 12))),
+          SizedBox(width: 10),
           Expanded(
-            flex: 2,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                CustomText(
-                 content: leadCustomerData.address ?? '',
-                 fontSize: 12,
-                  maxLine: 2,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                CustomText(
-                 content: leadCustomerData.town ?? '',
-                  fontSize: 12,
-                  maxLine: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                CustomText(
-                content:  leadCustomerData.state ?? '',
-                 fontSize: 12,
-                  maxLine: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                CustomText(
-                content:  leadCustomerData.zipcode?.toString() ?? '',
-                 fontSize: 12,
-                  maxLine: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ],
-            ),
-          ),
+              child: Center(
+                  child: CustomText(
+                      content: leadCustomerData.email ?? '', fontSize: 12))),
           Expanded(
-            child: Center(
-                child: CustomText(
-                 content: leadCustomerData.businessNo ?? '',
-                   fontSize: 12)),
-          ),
-          SizedBox(width: 10,),
+              child: Center(
+                  child: CustomText(
+                      content: leadCustomerData.fullname ?? '', fontSize: 12))),
           Expanded(
-            child: Center(
-                child: CustomText(content:leadCustomerData.email ?? '',
-                    fontSize: 12)),
-          ),
+              child: Center(
+                  child: CustomText(
+                      content: leadCustomerData.mobileno ?? '', fontSize: 12))),
           Expanded(
-            child: Center(
-                child: CustomText(content : leadCustomerData.fullname ?? '',
-                    fontSize: 12)),
-          ),
+              child: LeadsStatusSelect(customerId: leadCustomerData.id ?? 0)),
           Expanded(
-            child: Center(
-                child: CustomText(content:leadCustomerData.mobileno ?? '',
-                   fontSize: 12)),
-          ),
-          Expanded(
-            child: LeadsStatusSelect(customerId: leadCustomerData.id ?? 0),
-          ),
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(width: 5),
-                SizedBox(
-                  width: 20,
-                  child: IconButton(
-                    onPressed: () {
-                      EditLeadsDialog.showEditLeadsDialog(
-                          context, leadCustomerData);
-                    },
-                    padding: EdgeInsets.all(4),
-                    constraints: const BoxConstraints(),
-                    icon: const Icon(EneftyIcons.edit_outline,size: 20,),
-                  ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: SizedBox(
+                height: fixedRowHeight-10,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    SizedBox(
+                      width: 20,
+                      child: IconButton(
+                        onPressed: () {
+                          EditLeadsDialog.showEditLeadsDialog(
+                              context, leadCustomerData);
+                        },
+                        padding: EdgeInsets.all(2),
+                        constraints: const BoxConstraints(),
+                        icon: const Icon(EneftyIcons.edit_outline, size: 20),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 20,
+                      child: IconButton(
+                        onPressed: () {},
+                        padding: EdgeInsets.all(2),
+                        constraints: const BoxConstraints(),
+                        icon: const Icon(EneftyIcons.trash_outline,
+                            color: Colors.red, size: 20),
+                      ),
+                    ),
+                  ],
                 ),
-                SizedBox(width: 6),
-                SizedBox(
-                  width: 20,
-                  child: IconButton(
-                    onPressed: () {},
-                    padding: EdgeInsets.all(4),
-                    constraints: const BoxConstraints(),
-                    icon: const Icon(EneftyIcons.trash_outline, color: Colors.red, size: 20),
-                  ),
-                ),
-              ],
+              ),
             ),
           )
         ],
@@ -229,6 +314,7 @@ class LeadBottomScreen extends StatelessWidget {
     );
   }
 }
+
 
 class EditLeadsDialog extends StatelessWidget {
   final LeadCustomerData leadCustomerData;

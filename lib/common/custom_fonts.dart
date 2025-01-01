b3503ebf092f01c4aaf7526d962d653224a346ed
muhammed-ajmal1217 <stranger99980@gmail.com>
2025-01-01 15:59:35 +0530

    some changes

--- lib/common/custom_fonts.dart
+++ lib/common/custom_fonts.dart
@@ -97,16 +97,14 @@ class DialogTableHeaderText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Text(
-        text,
-        textAlign: align,
-        style: TextStyle(
-          color: Colors.black,
-          fontWeight: FontWeight.w600,
-          fontSize: fontSize,
-          fontFamily: 'Poppins_Regular',
-        ),
+    return Text(
+      text,
+      textAlign: align,
+      style: TextStyle(
+        color: Colors.black,
+        fontWeight: FontWeight.w600,
+        fontSize: fontSize,
+        fontFamily: 'Poppins_Regular',
       ),
     );
   }

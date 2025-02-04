c9dce79f08d195e70e416dd96a714beffad9720f
muhammed-ajmal1217 <stranger99980@gmail.com>
2025-02-04 15:04:40 +0530

    some changes

--- lib/ui/components/side_bar/nk_sidebar_only_icon.dart
+++ lib/ui/components/side_bar/nk_sidebar_only_icon.dart
@@ -287,7 +287,7 @@ void handleBackNavigation(
                     Navigator.of(context, rootNavigator: true).pop();
                     CartDatabaseManager().cartItems.clear();
                     CartDatabaseManager().cartPreorderItems.clear();
-                    CartDatabaseManager().clearCart('');
+                    CartDatabaseManager().clearCartOnSave('');
                     CartDatabaseManager().clearPreorderCart();
                     log('Cart Cleared');
                     updateTabIndex();

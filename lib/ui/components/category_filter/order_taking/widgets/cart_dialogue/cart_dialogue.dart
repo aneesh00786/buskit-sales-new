53522d17b8e21e326472e91783c9a7c90f53e7b1
muhammed-ajmal1217 <stranger99980@gmail.com>
2025-02-22 15:05:32 +0530

    some changes

--- lib/ui/components/category_filter/order_taking/widgets/cart_dialogue/cart_dialogue.dart
+++ lib/ui/components/category_filter/order_taking/widgets/cart_dialogue/cart_dialogue.dart
@@ -24,6 +24,7 @@ import 'package:busskit_salesexecutive/ui/theme/custom_toast_alert.dart';
 import 'package:busskit_salesexecutive/ui/utills/extentions/string_extention.dart';
 import 'package:busskit_salesexecutive/ui/view/ui/customer_and_orders/cus_provider/cus_provider.dart';
 import 'package:busskit_salesexecutive/ui/view/ui/customer_and_orders/customer_and_orders_controller.dart';
+import 'package:busskit_salesexecutive/ui/view/ui/dashboard1/provider/dash_provider.dart';
 import 'package:busskit_salesexecutive/ui/view/ui/products/products_controller.dart';
 import 'package:dio/dio.dart';
 import 'package:enefty_icons/enefty_icons.dart';
@@ -1352,6 +1353,7 @@ class CartDialogueState extends State<CartDialogue> {
                                   ? customeController.customerId.value
                                   : widget.productsController.selectedCustomerId
                                       .value);
+                          Provider.of<DashboardProvider>(context, listen: false).fetchData();
                           _clearCartItem(itemList, true);
                         },
                         child: const Text('OK'),

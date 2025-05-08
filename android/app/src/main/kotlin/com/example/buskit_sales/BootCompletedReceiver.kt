39ab19ee1fcc6457a38a3009575927863ee1de31
muhammed-ajmal1217 <stranger99980@gmail.com>
2025-05-08 13:34:05 +0530

    some updates

---
+++ android/app/src/main/kotlin/com/example/buskit_sales/BootCompletedReceiver.kt
@@ -0,0 +1,14 @@
+package com.example.buskit_sales
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
+class BootCompletedReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (Intent.ACTION_BOOT_COMPLETED == intent.action) {
+            Log.d("BootCompletedReceiver", "Device boot completed!")
+        }
+    }
+}

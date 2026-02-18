d42111f312b25cdc863a9ee8f82ad7ec060dc9cf
abhirammadhumadhav <abhirammadhunote@gmail.com>
2026-02-18 16:24:35 +0530

    added missed events onTap

--- lib/ui/view/ui/performance_screen/model/visit_report_model.dart
+++ lib/ui/view/ui/performance_screen/model/visit_report_model.dart
@@ -2,13 +2,14 @@ class VisitReportModel {
   DateTime? date;
   int visited, missed, total;
   final String eventIds;
-
+  final String missedEventIds;
   VisitReportModel({
     this.date, 
     this.visited = 0, 
     this.missed = 0, 
     this.total = 0,
     required this.eventIds,
+    required this.missedEventIds,
   });
 
   factory VisitReportModel.fromJson(Map<String, dynamic> json) {
@@ -20,9 +21,8 @@ class VisitReportModel {
       visited: counts["visited"] ?? 0,
       missed: counts["missed"] ?? 0,
       total: counts["total_visit"] ?? 0,
-      
-    
       eventIds: counts['visited_event_ids'] ?? '', 
+      missedEventIds: counts['missed_event_ids'] ?? '',
     );
   }
 }

727daadcee272f23183aff0e8a47bae35df788b9
Aneesh <aneesh.j4source@gmail.com>
2026-07-31 19:30:12 +0530

    updated baseurl

--- lib/api_handler/api_constants.dart
+++ lib/api_handler/api_constants.dart
@@ -1,5 +1,5 @@
 mixin class ApiConstants {
-  static const String baseUrl1 = "https://test.thrivewoo.com";
+  static const String baseUrl1 = "https://thrivewoo.com";
   static const String baseUrl = "$baseUrl1/";
   static const String imageBaseUrl = "${baseUrl}uploads/";
   static const String imageBaseUrlss = "${baseUrl}uploads";
